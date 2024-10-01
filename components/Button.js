64bc353350618ed9ba259c35153b6412e5ca3f41
Johnny Leyva <johnny.leyva182@gmail.com>
2024-09-30 22:55:20 -0700

    Fonts!

---
+++ components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Button(props) {
+    const { text } = props
+  return (
+    <Button>
+        {text}
+    </Button>
+  )
+}
--- components/Hero.js
+++ components/Hero.js
@@ -5,9 +5,12 @@ const fugaz = Fugaz_One({ subsets: ["latin"], weight: ['400'] });
 
 export default function Hero() {
   return (
-    <div className='py-10 sm:py-14 md:py-20'>
+    <div className='py-4 md:py-10 flex flex-col gap-4 sm:gap-8'>
       <h1 className={'text-5xl sm:text-text-6xl md:text-7xl text-center ' + fugaz.className}><span
       className='textGradient'>Gym Track</span> helps you track your <span className='textGradient'>daily</span> workouts!</h1>
+      <p className='text-lg sm:text-xl md:text-2xl text-center w-full mx-auto max-w-[600px]'
+      >Track your workout record and see how you did 
+        <span className='font-semibold'> every day of every year!</span></p>
     </div>
   )
 }
